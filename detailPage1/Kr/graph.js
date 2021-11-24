f5b8d63b94855e7828c2deb25a4ecea1a13b1a1b
ym1018 <73922043+ym1018@users.noreply.github.com>
2021-11-25 03:24:06 +0900

    detail Page 1

---
+++ detailPage1/Kr/graph.js
@@ -0,0 +1,52 @@
+var canvasElement = document.getElementById("KrChart");
+
+const makeData = (items) =>{
+    const arr = items.reduce((acc,cur)=>{
+        const currentDate = new Date(cur.Date);
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const date = currentDate.getDate();
+        const confirmed = cur.Confirmed;
+        const active = cur.Active;
+        const death = cur.Deaths;
+        const recovered = cur.Recovered;
+
+        acc.push({
+            year : year,
+            month : month,
+            date : date,
+            confirmed : confirmed,
+            active : active,
+            death : death,
+            recovered : recovered
+        })
+
+        return acc;
+        
+    },[])
+    
+    // 그래프 가공된 데이터 넣기
+    const labels1 = arr.map(a=> `${a.year}년 ${a.month+1}월 ${a.date}일`);
+    var config1 = {
+        type: 'line',
+        data: {
+            labels: labels1,
+            datasets: [{
+                label: "한국",
+                        backgroundColor: "salmon",
+                        fill: false,
+                        data: arr.map(a => a.death)
+                }]
+            },
+    };
+    var KrChart = new Chart(canvasElement,config1);
+}
+
+
+//var testChart = new Chart(canvasElement,config);
+console.log("next is api level")
+
+fetch("https://api.covid19api.com/total/dayone/country/kr")
+  .then((response) => response.json())
+  .then((data) =>  makeData(data));
+ 
